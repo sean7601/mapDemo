f0c132754a509cf9d62fd37136003f8752fa6b78
Sean Lavelle <sean.m.lavelle@navy.mil>
2020-10-21 15:58:43 -0400

    added track to match

--- randomPath.js
+++ randomPath.js
@@ -1,3 +1,21 @@
+function llFromDistance(latitude, longitude, distance, bearing) {
+    // taken from: https://stackoverflow.com/a/46410871/13549 
+    // distance in KM, bearing in degrees
+    distance *= 1.852;//converts to KM
+    const R = 6378.1; // Radius of the Earth
+    const brng = bearing * Math.PI / 180; // Convert bearing to radian
+    let lat = latitude * Math.PI / 180; // Current coords to radians
+    let lon = longitude * Math.PI / 180;
+  
+    // Do the math magic
+    lat = Math.asin(Math.sin(lat) * Math.cos(distance / R) + Math.cos(lat) * Math.sin(distance / R) * Math.cos(brng));
+    lon += Math.atan2(Math.sin(brng) * Math.sin(distance / R) * Math.cos(lat), Math.cos(distance / R) - Math.sin(lat) * Math.sin(lat));
+  
+    // Coords back to degrees and return
+    return [(lat * 180 / Math.PI), (lon * 180 / Math.PI)];
+  }
+
+
 const trackQuantity = 50;
 let startPoints = [];
 for(let i=0;i<trackQuantity;i++){
@@ -32,11 +50,12 @@ for(let i=0;i<trackQuantity;i++){
 }
 
 
-let connectedPaths = [];
+let historicalPaths = [];
 for(let i=0;i<trackQuantity;i++){
     let start = {x:startPoints[i].lng,y:startPoints[i].lat}
     let intermediate = {x:intermediatePoints[i].lng,y:intermediatePoints[i].lat}
     let end = {x:endPoints[i].lng,y:endPoints[i].lat}
+
     let firstGenerator = new arc.GreatCircle(start, intermediate, {'name': i+"first"});
     let secondGenerator = new arc.GreatCircle(intermediate, end, {'name': i+"second"});
     let firstLine = firstGenerator.Arc(10);
@@ -52,9 +71,37 @@ for(let i=0;i<trackQuantity;i++){
     }
 
     L.polyline(result, {color: 'red',weight:.3}).addTo(map);
-    connectedPaths.push(result)
+    historicalPaths.push(result)
 }
 
 
-console.log(connectedPaths)
+console.log(historicalPaths)
+
+
+
+//-105 to -90
+// +10 to -13
+let lat = 10 - 23 * Math.random();
+let lng = -105 + 15 * Math.random();
+let trackStart = [lat,lng]
+L.circle(trackStart,{color:"black"}).addTo(map)
+
+let trackEnd = llFromDistance(lat, lng, 50 + Math.random() * 100, 220 + Math.random() * 100)
+L.circle(trackEnd,{color:"purple"}).addTo(map)
+
+let start = {x:trackStart[1],y:trackStart[0]};
+let end = {x:trackEnd[1],y:trackEnd[0]};
+let trackGenerator = new arc.GreatCircle(start, end, {'name': "track"});
+let trackLine = trackGenerator.Arc(10);
+
+let observedTrack = [];
+for(var i=0;i<trackLine.geometries[0].coords.length;i++){
+    let coord = trackLine.geometries[0].coords[i];
+    observedTrack.push([coord[1],coord[0]]);
+}
+
+L.polyline(observedTrack, {color: 'magenta',weight:1}).addTo(map);
+
+console.log(observedTrack)
+
 
